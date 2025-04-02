6f4a7d0ad5ae44eb7789d37cb3a4a41211ec93b1
Guillermo Esteban Diaz Quezada <guillermo.diaz@3it.cl>
2025-04-02 17:50:16 -0300

    Se sube test de GetAllTrinitianosUseCaseImplTest

---
+++ backend/mscv-trinitianos/src/test/java/com/service3it/mscv_trinitianos/application/usecase/GetAllTrinitianosUseCaseImplTest.java
@@ -0,0 +1,63 @@
+package com.service3it.mscv_trinitianos.application.usecase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.service3it.mscv_trinitianos.domain.models.Trinitianos;
+import com.service3it.mscv_trinitianos.domain.ports.out.TrinitianosRepositoryPort;
+
+public class GetAllTrinitianosUseCaseImplTest {
+
+    @Mock
+    private TrinitianosRepositoryPort trinitianosRepositoryPort;
+
+    @InjectMocks
+    private GetAllTrinitianosUseCaseImpl getAllTrinitianosUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldReturnAllTrinitarios() {
+        // Arrange
+        List<Trinitianos> mockTrinitarios = Arrays.asList(
+            new Trinitianos(1L, "Juan", "Perez", "juan.perez@example.com", "12345678-9", "+12345678901", "Activo", null, null, null, null, null),
+            new Trinitianos(2L, "Maria", "Gomez", "maria.gomez@example.com", "87654321-K", "+12345678902", "Inactivo", null, null, null, null, null)
+        );
+        when(trinitianosRepositoryPort.getAllTrinitianos()).thenReturn(mockTrinitarios);
+
+        // Act
+        List<Trinitianos> result = getAllTrinitianosUseCase.getAllTrinitianos();
+
+        // Assert
+        assertEquals(mockTrinitarios.size(), result.size());
+        assertEquals(mockTrinitarios, result);
+        verify(trinitianosRepositoryPort, times(1)).getAllTrinitianos();
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenNoTrinitariosFound() {
+        // Arrange
+        when(trinitianosRepositoryPort.getAllTrinitianos()).thenReturn(List.of());
+
+        // Act
+        List<Trinitianos> result = getAllTrinitianosUseCase.getAllTrinitianos();
+
+        // Assert
+        assertEquals(0, result.size());
+        verify(trinitianosRepositoryPort, times(1)).getAllTrinitianos();
+    }
+}
