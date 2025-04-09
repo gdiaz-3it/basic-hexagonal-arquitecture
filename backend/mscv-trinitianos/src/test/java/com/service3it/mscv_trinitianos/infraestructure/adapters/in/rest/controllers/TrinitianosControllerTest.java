93562535852040210102cf84e48e1de7bf148b3a
Guillermo Esteban Diaz Quezada <guillermo.diaz@3it.cl>
2025-04-09 15:15:19 -0400

    Se borran archivos que no se usan como el .json

--- backend/mscv-trinitianos/src/test/java/com/service3it/mscv_trinitianos/infraestructure/adapters/in/rest/controllers/TrinitianosControllerTest.java
+++ backend/mscv-trinitianos/src/test/java/com/service3it/mscv_trinitianos/infraestructure/adapters/in/rest/controllers/TrinitianosControllerTest.java
@@ -28,8 +28,8 @@ import java.util.Arrays;
 import java.util.List;
 
 @WebMvcTest(TrinitianosController.class)
-@Import(TrinitianosControllerUnitTest.MockConfig.class)
-public class TrinitianosControllerUnitTest {
+@Import(TrinitianosControllerTest.MockConfig.class)
+public class TrinitianosControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
