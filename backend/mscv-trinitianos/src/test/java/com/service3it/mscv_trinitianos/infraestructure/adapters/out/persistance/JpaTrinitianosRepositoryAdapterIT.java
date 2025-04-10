29c10944f5e75e920ba471bcf3213b76ca2a0bab
Guillermo Esteban Diaz Quezada <guillermo.diaz@3it.cl>
2025-04-10 16:37:27 -0400

    Se elimina carpeta .idea

--- backend/mscv-trinitianos/src/test/java/com/service3it/mscv_trinitianos/infraestructure/adapters/out/persistance/JpaTrinitianosRepositoryAdapterIT.java
+++
@@ -1,8 +0,0 @@
-package com.service3it.mscv_trinitianos.infraestructure.adapters.out.persistance;
-
-import org.springframework.test.context.TestPropertySource;
-
-@TestPropertySource("classpath:application-test.properties")
-public class JpaTrinitianosRepositoryAdapterIT {
-
-}
