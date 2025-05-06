697a8e45f37c9c5dd92422b82fc856f64c61b960
Guillermo Esteban Diaz Quezada <guillermo.diaz@3it.cl>
2025-05-06 16:43:53 -0400

    Se eliminan los mcsv-aplicaciones y mcsv-lenguajes ya que no se ocuparan

--- backend/mcsv-lenguajes/src/main/java/com/service3it/mcsv_lenguajes/domain/ports/in/GetLenguajesByIdUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.service3it.mcsv_lenguajes.domain.ports.in;
-
-import com.service3it.mcsv_lenguajes.domain.models.Lenguajes;
-
-public interface GetLenguajesByIdUseCase {
-
-    Lenguajes getLenguajesById(Long id);
-
-}
--- backend/mcsv-lenguajes/src/main/java/com/service3it/mcsv_lenguajes/infraestructure/adapters/out/persistance/repositories/LenguajesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.service3it.mcsv_lenguajes.infraestructure.adapters.out.persistance.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.service3it.mcsv_lenguajes.infraestructure.adapters.out.persistance.entities.LenguajesEntity;
-
-public interface LenguajesRepository extends JpaRepository<LenguajesEntity, Long> {
-
-}
